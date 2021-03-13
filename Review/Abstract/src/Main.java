3d30334c3b0db3e6735c694ac29df1b4ef2a3b60
Thao <thaotruong199826@gmail.com>
2021-03-13 12:02:21 +0700

    review abstraction

---
+++ Review/Abstract/src/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author truongthao
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cho cho = new Cho();
+        cho.hienThi();
+        
+        Meo meo = new Meo();
+        meo.hienThi();
+    }
+    
+}
